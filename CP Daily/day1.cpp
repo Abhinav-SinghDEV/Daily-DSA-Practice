b90ac5c7a6ed5a3b7690af0ec69198381d9f2551
Abhinav Kumar Singh <pragatisinghniru20@gmail.com>
2026-03-16 22:00:50 +0530

    Implement solution for counting maximums in array

--- CP Daily/day1.cpp
+++ CP Daily/day1.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    int op = 0;
+    int max = -1;
+
+    for (int i = 0; i < n; i++) {
+        if (nums[i] >= max) {
+            max = nums[i];
+            op++;
+        }
+    }
+
+    cout << op<<endl;
+}
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
