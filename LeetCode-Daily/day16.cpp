561149f34e5eb3358c5e8e534b19529a22b24964
Abhinav Kumar Singh <pragatisinghniru20@gmail.com>
2026-05-03 22:24:32 +0530

    Implement rotateString function in day16.cpp

---
+++ LeetCode-Daily/day16.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal){
+        if(s.size()!=goal.size()) return false;
+        string temp=s+s;
+        return temp.find(goal)!=-1;
+
+
+
+
+
+        
+    }
+};
