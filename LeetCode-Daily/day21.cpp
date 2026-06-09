b8031047f33aa845e7554fe0d8a69fe503bf1054
Abhinav Kumar Singh <pragatisinghniru20@gmail.com>
2026-06-09 14:10:14 +0530

    Implement function to reverse words in a string

---
+++ LeetCode-Daily/day21.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+
+        int n = s.size();
+        string ans = "";
+
+        int i = n - 1;
+
+        while(i >= 0){
+
+            // skip spaces
+            while(i >= 0 && s[i] == ' ')
+                i--;
+
+            if(i < 0)
+                break;
+
+            string word = "";
+
+            // collect current word
+            while(i >= 0 && s[i] != ' '){
+                word = s[i] + word;
+                i--;
+            }
+            // add spaces if the current ans is not empty..
+            if(!ans.empty())
+                ans += " ";
+
+            ans += word;
+        }
+
+        return ans;
+    }
+};
