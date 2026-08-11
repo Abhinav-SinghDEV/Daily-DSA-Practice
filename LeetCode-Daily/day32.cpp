7ba7083f6821e15a4b66cbc64213550c9b155060
Abhinav Kumar Singh <pragatisinghniru20@gmail.com>
2026-08-11 19:55:01 +0530

    Add missingInteger function to find missing integer
    Implement a solution to find the missing integer in a sorted array.

---
+++ LeetCode-Daily/day32.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int missingInteger(vector<int>& nums) {
+        int n = nums.size();
+
+        int sum = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1] + 1)
+                sum += nums[i];
+            else
+                break;
+        }
+        sort(nums.begin(), nums.end());
+
+        int ans = sum;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == ans)
+                ans++;
+        }
+
+        return ans;
+    }
+};
